04525319f61ce5e9641ff4ec518cceb67f7f443f
racoding <fkxowls@gmail.com>
2023-02-17 18:45:33 +0900

    흑백과 바이너리 기능 구현

--- app.py
+++ app.py
@@ -1,26 +1,73 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
+from flask_cors import CORS
 from werkzeug.utils import secure_filename
 from PIL import Image
+import shutil
+import cv2
 
 
 
 app = Flask(__name__)
+CORS(app)
+
+originalSavePath = './static/img/original.png'
+preporcessingSavePath = './static/img/preprocessing.png'
+preprocessingMethod = "전처리 기법을 선택해 주세요"
+slideDisplay = "display:none"
+
 
 @app.route('/')
 def index():
-    return render_template('index copy.html')
+    return render_template('index.html', preprocessingMethod=preprocessingMethod, slideDisplay=slideDisplay)
+
 
 @app.route('/file_upload', methods=['GET', 'POST'])
 def file_upload():
     if request.method == 'POST':
         f = request.files['file']
         # img = Image.open(f)
-        image_save_path = './static/img/temp.png'
-        f.save(image_save_path)
+        f.save(originalSavePath)
+        shutil.copy(originalSavePath, preporcessingSavePath)
         
-    return render_template('index.html', image_file=image_save_path)
-   
-   
+    return render_template('index.html', preprocessingMethod=preprocessingMethod, originalImagePath=originalSavePath,
+                           slideDisplay=slideDisplay)
+
+
+@app.route('/color_to_gray', methods=['GET', 'POST'])
+def color_to_gray():
+    slideDisplay = "display:none"
+    if request.method == 'POST':
+        preprocessingMethod = "흑백 이미지 만들기"
+        img = cv2.imread(originalSavePath, cv2.IMREAD_GRAYSCALE)
+        cv2.imwrite(preporcessingSavePath, img)
+        
+    return render_template('index.html', preprocessingMethod=preprocessingMethod, originalImagePath=originalSavePath,
+                           preprocessingImagePath=preporcessingSavePath, slideDisplay=slideDisplay)
+
+
+@app.route('/make_binary', methods=['GET', 'POST'])
+def make_binary():
+    defaultSlideValue = 127
+    if request.method == 'POST':
+        preprocessingMethod = "바이너리로 만들기"
+        img = cv2.imread(originalSavePath, cv2.IMREAD_GRAYSCALE)
+        _, img = cv2.threshold(img, defaultSlideValue, 255, cv2.THRESH_BINARY)
+        cv2.imwrite(preporcessingSavePath, img)
+        
+    return render_template('index.html', preprocessingMethod=preprocessingMethod, originalImagePath=originalSavePath, 
+                           preprocessingImagePath=preporcessingSavePath, slideValue=defaultSlideValue)   
+
+
+@app.route('/slide_binary', methods=['POST'])
+def slide_binary():
+    slideValue = int(request.form['value'])
+    img = cv2.imread(originalSavePath, cv2.IMREAD_GRAYSCALE)
+    _, img = cv2.threshold(img, slideValue, 255, cv2.THRESH_BINARY)
+    cv2.imwrite(preporcessingSavePath, img)
+    return ""
+
+    
     
 if __name__ == '__main__':
-    app.run(debug=True)
+    # default port=5000
+    app.run(debug=True, port=5000)
--- static/js/script.js
+++ static/js/script.js
@@ -18,3 +18,46 @@ function uploadImage() {
     // 이미지 파일 읽기 시작
     reader.readAsDataURL(file);
 }
+
+function reloadImage() {
+  var imgElement = document.getElementById("preprocessingImage");
+  var src = imgElement.getAttribute("src");
+  // 매개변수를 추가하여 URL을 변경합니다.
+  //src = src + "?time=" + new Date().getTime();
+  src = src + "?1"
+  imgElement.setAttribute("src", src);
+}
+
+function slide_event() {
+  var slider = document.getElementById("slider");
+  var sliderValue = document.getElementById("sliderValue");
+  sliderValue.innerHTML = slider.value;
+  
+  slider.oninput = function() {
+      sliderValue.innerHTML = this.value;
+  }
+}
+
+function slide_event_handler() {
+  // 슬라이더 이벤트 핸들러 등록
+  document.getElementById("slider").addEventListener("input", function() {
+    // 서버로 슬라이더 값을 전송
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "/slide_binary", true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.send("value=" + this.value);
+    // 서버에서 반환한 이미지 업데이트
+    xhr.onreadystatechange = function() {
+        if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+            //document.getElementById("result").src = "data:image/jpeg;base64," + btoa(String.fromCharCode.apply(null, new Uint8Array(this.response)));
+            reloadImage();
+        }
+    }
+    // 슬라이더 값을 출력
+    document.getElementById("sliderValue").innerHTML = this.value;
+  });
+}
+
+function hidden_slide() {
+  document.getElementById("slideSector").style.display = "none";
+}
